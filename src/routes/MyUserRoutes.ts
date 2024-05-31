765ada6665a7bb00266847da619eaa7ddf01e512
hoamyxinhdep <103980605+hoamyxinhdep@users.noreply.github.com>
2024-05-31 14:20:51 +0700

    first commit

---
+++ src/routes/MyUserRoutes.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import MyUserController from "../controllers/MyUserController";
+import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyUserResquest } from "../middleware/validation";
+
+const router = express.Router();
+
+router.get("/", jwtCheck, jwtParse, MyUserController.getCurrentUser);
+router.post("/", jwtCheck, MyUserController.createCurrentUser);
+router.put(
+  "/",
+  jwtCheck,
+  jwtParse,
+  validateMyUserResquest,
+  MyUserController.updateCurrentUser
+);
+
+export default router;
