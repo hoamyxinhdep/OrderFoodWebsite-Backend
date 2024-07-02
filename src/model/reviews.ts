922d8c5ae4a17b3ed550df1c445094776ff3bdb6
tamminh <nguyendactamminh@gmail.com>
2024-07-02 22:30:04 +0700

    update code

--- src/model/reviews.ts
+++ src/model/reviews.ts
@@ -1,21 +1,32 @@
-import { Schema } from 'mongoose';
 import mongoose from "mongoose";
+import { Schema } from 'mongoose';
 
-const reviewSchema = new mongoose.Schema( {
-    user: {
-      type: Schema.Types.ObjectId,
-      ref: 'User',
-      required: true,
-    },
-    restaurant:{
-      type: Schema.Types.ObjectId,
-      ref: "Restaurant",
-      require: true,
-    },
-    comment: String,
-    reply: String,
+const replySchema = new mongoose.Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
   },
-  { timestamps: true },);
+  text: {
+    type: String,
+    required: true,
+  },
+}, { timestamps: true });
+
+const reviewSchema = new mongoose.Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  restaurant: {
+    type: Schema.Types.ObjectId,
+    ref: "Restaurant",
+    required: true,
+  },
+  comment: String,
+  replies: [replySchema],
+}, { timestamps: true });
 
 const Review = mongoose.model("Review", reviewSchema);
 export default Review;
